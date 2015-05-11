f68d063feb29623a846e3864476e057d10278464
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-05-10 22:19:43 -0700

    additional functions and formatting

--- app.js
+++ app.js
@@ -2,45 +2,60 @@
 
 // Execute this code only AFTER the document is ready
 // Hint: use jQuery's `$(document).ready`
-$(function() {
-  function Game(player1, player2) 
+$(document).ready(function() {
+
+  function Game(playerX, playerO) {
     //Create a new instance of player 1
-    this.player1 = player1
+    this.playerX = new Player(x);
+    console.log(playerX)
 
     //Do the same for a player 2
-    this.player2 = player2
+    this.playerO = new Player(o);
+    console.log(playerO)
     
     //Create the board
     this.board = function() {
-      
+      $("#board").html("<img src='chalkboard-assets/Board.png' />");
+      $(".box").each.html("$nbsp;");
     }
-  }
+  };
 
   // Remember: prototypes are shared functions between all game instances
   Game.prototype.nextPlayer = function() {
     //Switch players
+    if($)
   };
 
   // `Game.prototype.init` kicks off a new game with a board and two players
   Game.prototype.init = function() {
-    //
+    $(".player span")
   };
 
-  // A starter Player constructor.
+  // // A starter Player constructor.
   function Player(team) {
     //Is the player X or O?
-    //this.team = ...
+    this.team = team;
+    this.score = 0
   };
 
   // A starter Board constructor.
   function Board() {
     //Tracks the cells of the board instance
-    //this.$cells = ...
+    this.$cells = $(".box")
 
     //Store any other properties that board may have below, such as a reset option
+  };
+
+  
+
+  $("#newGame").on("click", function(event){
+    // Start the game!
+    console.log("Click works")
+    var game = new Game(x, o);
+    //game.board
+    //game.init();
+
   });
 
-  // Start the game!
-  var game = new Game();
-  game.init();
+
 });
